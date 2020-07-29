013f6baff89ab461d5072f25d970efe18e4f1629
devdave0 <dbchung24@gmail.com>
2020-07-29 12:11:12 -0400

    added create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,20 @@ class UsersController < ApplicationController
 
     def index 
         @users = User.all
-        
+
         render json: @users
     end 
+
+    def create 
+        @user = User.create(user_params)
+
+        render json: @user, status: :created
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name)
+    end 
+
 end
