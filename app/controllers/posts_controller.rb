de92a766968198dd4602e7aa14f0bcef7005cebb
devdave0 <dbchung24@gmail.com>
2020-07-30 16:20:33 -0400

    added post controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,18 @@ class PostsController < ApplicationController
     def index 
         @posts = Post.all 
 
-        render json: @posts
+        render json: @posts, include: :users, status: :ok
+    end 
+
+    def create 
+        @post = Post.create(post_params)
+
+        render json: @post, status: :created
+    end 
+
+    private 
+
+    def post_params 
+        params.require(:post).permit(:pic_url, :caption, :pet_id, :user_id)
     end 
 end
--- app/controllers/user_pets_controller.rb
+++ app/controllers/user_pets_controller.rb
@@ -5,4 +5,16 @@ class UserPetsController < ApplicationController
 
         render json: @user_pets
     end 
+
+    def create 
+        @user_pet = UserPet.create(user_pet_params)
+
+        render json: @user_pet, status: :created
+    end 
+
+    private 
+
+    def user_pet_params 
+        params.require(:user_pet).permit(:user_id, :pet_id)
+    end 
 end
