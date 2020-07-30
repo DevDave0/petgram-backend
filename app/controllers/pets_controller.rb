0d461ea58f753bf1d9b86ba54c078a20a99cfc6a
devdave0 <dbchung24@gmail.com>
2020-07-30 11:21:17 -0400

    added pet controller actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,7 +3,19 @@ class PetsController < ApplicationController
     def index 
         @pets = Pet.all 
 
-        render json: @pets 
+        render json: @pets, include: :users, status: :ok
+    end 
+
+    def create 
+        @pet = Pet.create(pet_params)
+
+        render json: @pet, status: :created
+    end 
+
+    private 
+
+    def pet_params 
+        params.require(:pet).permit(:name, :pic_url, :description, :animal)
     end 
     
 end
