0b9a1f909bfecc7d404f6544a1a081b8a22815f3
devdave0 <dbchung24@gmail.com>
2020-07-31 07:27:48 -0400

    added like actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,4 +5,24 @@ class LikesController < ApplicationController
 
         render json: @likes
     end 
+
+    def create 
+        @like = like.new 
+        @like.like = params[:like]
+        @like.user_id = params[:user_id]
+        @like.post_id = params[:post_id]
+        @like.save
+
+        render json: @like, status: :created
+    end 
+
+    def destroy 
+        @like = like.find_by(id: params[:id])
+        if @like
+            @like.destroy
+            render json: { messages: ["like was deleted!"] }, status: :ok
+        else
+            render json: { errors: ['like not found.'] }, status: :not_found
+        end
+    end 
 end
