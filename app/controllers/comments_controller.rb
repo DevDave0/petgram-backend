2712807c8823ee33e7b10ccb5e2fbd6cac37d6e0
devdave0 <dbchung24@gmail.com>
2020-07-30 21:01:07 -0400

    include comments to users and added comment actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,19 @@ class CommentsController < ApplicationController
     def index 
         @comments = Comment.all 
 
-        render json: @comments 
+        render json: @comments, include: :users, status: :ok
+    end 
+
+    def create 
+        @comment = Comment.create(comment_params)
+
+        render json: @comment, status: :created
+    end 
+
+    private 
+
+    def comment_params 
+        params.require(:comment).permit(:comment, :user_id, :post_id)
     end 
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def index 
         @users = User.all
 
-        render json: @users, include: :pets, status: :ok
+        render json: @users, :include => [:pets, :comments], status: :ok
     end 
 
     def create 
